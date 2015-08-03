53d578fdf59dfd735943c3c0f5ea3ad59f69c23a
João Paulo Dubas <joao.dubas@gmail.com>
2015-08-03 15:28:13 -0300

    [update] Document `required_auth` decorator.

--- salesforce/utils/decorators.py
+++ salesforce/utils/decorators.py
@@ -4,6 +4,20 @@ from ..exception import AuthenticationFailed
 
 
 def required_auth(func):
+    """required_auth -- Decorator that verifies if given class instance is
+    authenticated.
+
+    Args:
+        func (function): callable to be decorated
+
+    Raises:
+        (AuthenticationFailed): when authentication check fails.
+
+    Returns:
+        (function): wrapped callable where auth verification will be done before
+            calling decorated method.
+
+    """
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if self.auth is None or not self.auth.is_authenticated():
